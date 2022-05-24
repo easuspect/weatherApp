405dc9ae988882846e2009621d35bd9f7f1d734b
easuspect <31296818+easuspect@users.noreply.github.com>
2022-05-24 13:38:33 -0500

    SearchTextField updated and Added Segmented Control

--- WeatherAppProject/AddLocationViewController.swift
+++ WeatherAppProject/AddLocationViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class AddLocationViewController: UIViewController {
     
     var dataSource: SavedCityTableViewDataSource!
+    
+    @IBOutlet weak var segmentedLabel: UISegmentedControl!
 
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var searchImageView: UIImageView!
@@ -20,14 +22,22 @@ class AddLocationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+     
         dataSource = .init(tableView: tableView, vc: self)
+        findCityTextField.delegate = self
    
         containerView.backgroundColor = .clear
         containerView.layer.borderWidth = 1
         containerView.layer.borderColor = UIColor.white.cgColor
         containerView.layer.cornerRadius = 5
-        addButton.layer.cornerRadius = 5   
+        addButton.layer.cornerRadius = 5
+        
+        setPlaceHolderUI()
+        segmentedLabel.isHidden = true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
@@ -38,5 +48,33 @@ class AddLocationViewController: UIViewController {
         
     }
     
+    @IBAction func segmentedPressedButton(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            print("Listing")
+        } else {
+            print("Map")
+        }
+    }   
+}
+
+extension AddLocationViewController {
+    func setPlaceHolderUI() {
+        findCityTextField.attributedPlaceholder = NSAttributedString(
+            string: "Search",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray]
+        )
+    }
+}
+
+extension AddLocationViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        segmentedLabel.isHidden = false
+        print("textFieldDidBeginEditing")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        segmentedLabel.isHidden = true
+        print("End")
+    }
 }
 
